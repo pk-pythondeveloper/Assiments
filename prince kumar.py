035835f2bfc7a06f38586891d86fbf015ae16a64
prince kumar <pk5999972@gmail.com>
2024-04-23 01:51:57 +0530

    update data

--- prince kumar.py
+++ prince kumar.py
@@ -105,13 +105,22 @@ from array import*
 
 # 6.wrinte a programs to add two matrices
 
-x = [[1, 2, 3],
+X = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
 
-y = [[9, 8, 7],
+Y = [[9, 8, 7],
      [6, 5, 4],
      [3,2,1]]
 
-result1= [map(sum, zip(*t)) for t in zip(x, y)]
-print(result1)
+result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+   print(r)
